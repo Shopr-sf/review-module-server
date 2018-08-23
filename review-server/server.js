1c089d493cf1c7c088e5111218372e827fe043ba
OceanHelloWorld <oceanzyhy@gmail.com>
2018-08-22 17:03:35 -0700

    server ec2 setup

--- review-server/server.js
+++ review-server/server.js
@@ -1,17 +1,22 @@
-import redis from 'redis';
-import cors from 'cors';
-import path from 'path';
-import bodyParser from 'body-parser';
-import express from 'express';
-import {
-  getAll,
-  getAggregate,
-  getReviews,
-  getImages,
-  addReview,
-  updateReview,
-  deleteReview,
-} from './serverHelpers';
+// import redis from 'redis';
+// import cors from 'cors';
+var cors = require("cors");
+// import path from 'path';
+var bodyParser = require ("body-parser");
+// import bodyParser from 'body-parser';
+var redis = require("redis");
+var express = require("express");
+// import express from 'express';
+var getAll = require("./serverHelpers.js")
+// import {
+//   getAll,
+//   getAggregate,
+//   getReviews,
+//   getImages,
+//   addReview,
+//   updateReview,
+//   deleteReview,
+// } from './serverHelpers';
 // import newrelic from 'newrelic';
 // import { db } from '../review-database/connection';
 
@@ -81,59 +86,6 @@ app.get('*/reviews/all/:productId', (req, res, next) => {
     // console.log('PrOdUcT from promise', product);
     // console.log('SuMmArY from promise', summary);
     return res.send(summary);
-  });
-});
-
-
-app.get('*/reviews/summary/:productId', (req, res) => {
-  const product = Number(req.params.productId.replace(/[^0-9]/g, ''));
-	if (typeof product !== 'number') {
-    res.sendStatus(400);
-  }
-  getAggregate(product).then(summary => (res.send(summary)));
-});
-
-app.get('*/reviews/:productId', (req, res) => {
-  const product = Number(req.params.productId.replace(/[^0-9]/g, ''));
-	if (typeof product !== 'number') res.sendStatus(400);
-  getReviews(product, getImages).then(results => res.send(results));
-});
-
-app.get('*/reviews/comments/:reviewId', (req, res) => {
-  // TODO: add comment viewing
-  res.send();
-});
-
-app.post('*/reviews/adduser', (req, res) => {
-  const { username, img } = req.body;
-  addReview(username, img, (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.sendStatus(201);
-    return null;
-  });
-});
-
-app.put('*/reviews/updateuser', (req, res) => {
-  const { username, img, id } = req.body;
-  updateReview(username, img, id, (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.sendStatus(201);
-    return null;
-  });
-});
-
-app.delete('*/reviews/deleteuser', (req, res) => {
-  const { id } = req.body;
-  deleteReview(id, (err) => {
-    if (err) {
-      return res.status(500).send(err);
-    }
-    res.sendStatus(201);
-    return null;
   });
 });
 
--- review-server/serverHelpers.js
+++ review-server/serverHelpers.js
@@ -1,9 +1,8 @@
+var Promise = require('bluebird');
+// import Promise from 'bluebird';
 
-import Promise from 'bluebird';
-
-import { db } from '../review-database/connection';
-
-import { pool } from '../postgres-database/connection';
+const { pool } = require('../postgres-database/connection.js');
+// import { pool } from '../postgres-database/connection';
 // import { generateUsername } from '../review-database/seedHelpers';
 
 // getAggregate is a function that passes in 'prouctId' as parameter, and ouputs a promise
@@ -18,87 +17,87 @@ const getAll = productId => new Promise((resolve) => {
 });
 
 
-const getAggregate = productId => new Promise((resolve) => {
-  db.query(`SELECT * FROM aggregates WHERE product_id=${productId};`, (err, data) => {
-    if (err) return 404;
-    resolve(data);
-    return null;
-  });
-});
+// const getAggregate = productId => new Promise((resolve) => {
+//   db.query(`SELECT * FROM aggregates WHERE product_id=${productId};`, (err, data) => {
+//     if (err) return 404;
+//     resolve(data);
+//     return null;
+//   });
+// });
 
-const getReviews = (product, callback) => new Promise((resolve) => {
-  console.log(product);
-  db.query(`SELECT * FROM reviews INNER JOIN users ON reviews.user_id=users.id WHERE product_id=${product}`, (err, data) => {
-    if (err) return 404;
-    resolve(callback(data));
-    return null;
-  });
-});
+// const getReviews = (product, callback) => new Promise((resolve) => {
+//   console.log(product);
+//   db.query(`SELECT * FROM reviews INNER JOIN users ON reviews.user_id=users.id WHERE product_id=${product}`, (err, data) => {
+//     if (err) return 404;
+//     resolve(callback(data));
+//     return null;
+//   });
+// });
+
+// // const getImages = (reviews) => {
+// //   const results = {};
+// //   results.reviews = reviews;
+// //   let queryString = '';
+// //   return new Promise((resolve) => {
+// //     reviews.forEach((review) => {
+// //       queryString += `SELECT review_id, title, url FROM images WHERE review_id=${review.id};`;
+// //     });
+// //     db.query(queryString, (err, data) => {
+// //       if (err) return 404;
+// //       results.images = data;
+// //       resolve(results);
+// //       return null;
+// //     });
+// //   });
+// // };
+
 
 // const getImages = (reviews) => {
 //   const results = {};
 //   results.reviews = reviews;
-//   let queryString = '';
 //   return new Promise((resolve) => {
-//     reviews.forEach((review) => {
-//       queryString += `SELECT review_id, title, url FROM images WHERE review_id=${review.id};`;
-//     });
-//     db.query(queryString, (err, data) => {
-//       if (err) return 404;
-//       results.images = data;
-//       resolve(results);
-//       return null;
-//     });
+//     results.images = [[{ url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }]];
+//     resolve(results);
 //   });
 // };
 
 
-const getImages = (reviews) => {
-  const results = {};
-  results.reviews = reviews;
-  return new Promise((resolve) => {
-    results.images = [[{ url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }, { url: 'images (18) copy.jpeg' }]];
-    resolve(results);
-  });
-};
+// // TODO: complete
+// const getComments = review => new Promise((resolve) => {
+//   db.query(`SELECT * FROM comments WHERE review_id=${review};`, (err, data) => {
+//     if (err) return 404;
+//     resolve(data);
+//     return null;
+//   });
+// });
+
+// // TODO: complete
+// const addReview = (username, img, callback) => {
+// // add users record if new
+// // add reviews record (w/ foreign key user_id)
+// // add images record if applicable (w/ foreign key review_id)
+// // update/get aggregates
+//   db.query('INSERT INTO users(username, img) VALUES(?, ?)', [username, img], callback);
+// };
 
 
-// TODO: complete
-const getComments = review => new Promise((resolve) => {
-  db.query(`SELECT * FROM comments WHERE review_id=${review};`, (err, data) => {
-    if (err) return 404;
-    resolve(data);
-    return null;
-  });
-});
+// // TODO: complete
+// const updateReview = (username, img, id, callback) => {
+//   db.query('UPDATE users SET username = ?, img = ? WHERE (id = ?)', [username, img, id], callback);
+// // increment/decrement helpful, not_helpful, or abuse in review record
+// };
 
-// TODO: complete
-const addReview = (username, img, callback) => {
-// add users record if new
-// add reviews record (w/ foreign key user_id)
-// add images record if applicable (w/ foreign key review_id)
-// update/get aggregates
-  db.query('INSERT INTO users(username, img) VALUES(?, ?)', [username, img], callback);
-};
+// const deleteReview = (id, callback) => {
+//   db.query('DELETE FROM users WHERE id = ?', [id], callback);
+// };
 
-
-// TODO: complete
-const updateReview = (username, img, id, callback) => {
-  db.query('UPDATE users SET username = ?, img = ? WHERE (id = ?)', [username, img, id], callback);
-// increment/decrement helpful, not_helpful, or abuse in review record
-};
-
-const deleteReview = (id, callback) => {
-  db.query('DELETE FROM users WHERE id = ?', [id], callback);
-};
-
-export {
-  getAll,
-  getAggregate,
-  getReviews,
-  getImages,
-  getComments,
-  addReview,
-  updateReview,
-  deleteReview,
-};
+// export {
+//   getAll,
+//   getAggregate,
+//   getReviews,
+//   getImages,
+//   getComments,
+//   addReview,
+//   updateReview,
+//   deleteReview,
+// };
